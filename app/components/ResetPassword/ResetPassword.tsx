042525e3946f71e35f6968855024b6892aceff23
roopika-19 <roopikasaxena19@gmail.com>
2024-05-14 15:01:14 +0530

    heyy

--- app/components/ResetPassword/ResetPassword.tsx
+++ app/components/ResetPassword/ResetPassword.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { useState } from "react";
 import {
 	Paper,
 	Title,
@@ -9,34 +11,67 @@ import {
 	Box,
 	rem,
 	PasswordInput,
+	Input,
 } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import classes from "./ResetPassword.module.css";
 
 const ResetPassword = () => {
+	const [data, setData] = useState({
+		password: "",
+		newPassword: ""
+	});
+
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const confirmPassword = () => {
+		const { password, newPassword } = data;
+		if ( password ==="") {
+			setErrorMessage("Enter Valid Password");
+		} else {
+			setErrorMessage("");
+		}
+
+		if ( password !== newPassword) {
+			setErrorMessage("Passwords don't match");
+		} else {
+			setErrorMessage("");
+		}
+	};
+
+	const handleChange = (e: { target: { name: any; value: any; }; }) => {
+		const { name, value } = e.target;
+		setData((prevData) => ({
+		  ...prevData,
+		  [name]: value,
+		}));
+	};
+
 	return (
 		<Container size={460} my={30}>
 			<Title className={classes.title} ta="center" c="red">
 				IMPORTANT!! <br /> RESET YOUR PASSWORD
 			</Title>
 			<Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-				<PasswordInput label="New Password" required mb={10} />
-				<PasswordInput label="Verify New Password" required />
-				<Group
-					justify="space-between"
-					mt="lg"
-					className={classes.controls}
-				>
-					<Anchor c="dimmed" size="sm" className={classes.control}>
-						<Center inline>
-							<IconArrowLeft
-								style={{ width: rem(12), height: rem(12) }}
-								stroke={1.5}
-							/>
-							<Box ml={5}>Back to the login page</Box>
-						</Center>
-					</Anchor>
-					<Button className={classes.control}>Reset password</Button>
+				<PasswordInput
+					label="New Password"
+					name="password"
+					required
+					mb={10}
+					value={data.password}
+					onChange={handleChange}
+				/>
+				{errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+				<PasswordInput
+				    label="Verify New Password"
+					name="newPassword"
+					required
+					value={data.newPassword}
+					onChange={handleChange}
+				/>
+				
+				<Group justify="space-between" mt="lg" className={classes.controls}>
+					<Button className={classes.control} onClick={confirmPassword}>Reset password</Button>
 				</Group>
 			</Paper>
 		</Container>
